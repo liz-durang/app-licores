1cd750bce085fa1ef8388d82e39d04b7d0b2db8c
liiz-durang <lizeth.durang@gmail.com>
2023-12-05 20:40:36 -0600

    front v2 done

---
+++ frontendV2/src/products.js
@@ -0,0 +1,76 @@
+import img1 from './assets/vodka_absolut.webp'
+import img2 from './assets/cognac.webp'
+import img3 from './assets/tequila_maestro.webp'
+import img4 from './assets/ron_bacardi.webp'
+import img5 from './assets/tequila_don_ramon.webp'
+import img6 from './assets/whisky_yamaki.webp'
+import img7 from './assets/whisky_chivas.webp'
+import img8 from './assets/paquete_tezon.webp'
+import img9 from './assets/brandy_alma.webp'
+
+export const products = [
+    {
+        id: 1, 
+        nameProduct: 'VODKA ABSOLUT 750 ML',
+        price: 214.55,
+        urlImage: img1,
+        quantity: 1, 
+    }, 
+    {
+        id: 2, 
+        nameProduct: 'COGNAC MARTELL 700 ML',
+        price: 1100.05,
+        urlImage: img2,
+        quantity: 1, 
+    }, 
+    {
+        id: 3, 
+        nameProduct: 'TEQUILA DIAMANTE 700 ML',
+        price: 688.15,
+        urlImage: img3,
+        quantity: 1, 
+    }, 
+    {
+        id: 4, 
+        nameProduct: 'RON BACARDI AÑEJO 980 ML',
+        price: 236.25,
+        urlImage: img4,
+        quantity: 1, 
+    }, 
+    {
+        id: 5, 
+        nameProduct: 'TEQUILA DON RAMÓN 750 ML',
+        price: 514.55,
+        urlImage: img5,
+        quantity: 1, 
+    }, 
+    {
+        id: 6, 
+        nameProduct: 'WHISKY YAMAZAKI 750 ML',
+        price: 7500.55,
+        urlImage: img6,
+        quantity: 1, 
+    }, 
+    {
+        id: 7, 
+        nameProduct: 'WHISKY CHIVAS 750 ML',
+        price: 6650.10,
+        urlImage: img7,
+        quantity: 1, 
+    }, 
+    {
+        id: 8, 
+        nameProduct: 'PAQUETE TEQ. TEZON 750 ML',
+        price: 2250.50,
+        urlImage: img8,
+        quantity: 1, 
+    }, 
+    {
+        id: 9, 
+        nameProduct: 'BRANDY ALMA 700 ML',
+        price: 718.50,
+        urlImage: img9,
+        quantity: 1, 
+    }, 
+
+]
