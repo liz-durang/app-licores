5dd26f2d3439ac757c00f32eb3e82f0007abd5af
Liz Durán <42351387+liiz-durang@users.noreply.github.com>
2023-12-06 02:46:04 -0600

    Update insert_licores.sql

--- bd/insert_licores.sql
+++ bd/insert_licores.sql
@@ -1,7 +1,7 @@
 INSERT INTO licores (licor_id, product_name, price, image_url, quantity)
 VALUES
     (1, 'VODKA ABSOLUT 750 ML', 214.55, 'vodka_absolut.webp', 1),
-    (2, 'COGNAC MARTELL 700 ML', 1100.05, '/cognac.webp', 1),
+    (2, 'COGNAC MARTELL 700 ML', 1100.05, 'cognac.webp', 1),
     (3, 'TEQUILA DIAMANTE 700 ML', 688.15, 'tequila_maestro.webp', 1),
     (4, 'RON BACARDI AÑEJO 980 ML', 236.25, 'ron_bacardi.webp', 1),
     (5, 'TEQUILA DON RAMÓN 750 ML', 514.55, 'tequila_don_ramon.webp', 1),
